ff48d3dc496daaa0e59187240ce244671479d4a2
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-08-04 22:47:33 +0100

    Initial Commit

---
+++ FirstContact/FirstContactApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstContactApp.swift
+//  FirstContact
+//
+//  Created by Bruce Gilmour on 2021-08-04.
+//
+
+import SwiftUI
+
+@main
+struct FirstContactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
